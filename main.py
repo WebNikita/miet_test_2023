c4cfc26f7347197e08b88b3f735bfef2297253c7
n.shvora <n.shvora@raceit.ru>
2023-12-12 19:10:31 +0300

    Create calculate_average_grade function

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 student_data = [
-    {"name": "Андрей", "last_name": "Попов", "grade": "5", "project_id": "1"},
-    {"name": "Степан", "last_name": "Васильев", "grade": "", "project_id": "2"},
-    {"name": "Владимир", "last_name": "Хадаров", "grade": "4", "project_id": "3"}
+    {"name": "Андрей", "surname": "Попов", "grade": 5, "project_id": "1"},
+    {"name": "Степан", "surname": "Васильев", "grade": None, "project_id": "2"},
+    {"name": "Владимир", "surname": "Хадаров", "grade": 4, "project_id": "3"}
 ]
+
+def calculate_average_grade(grades):
+    """
+    Вычисляем среднюю оценку из списка оценок
+
+    Args:
+    grades (list): Список оценок.
+
+    Returns:
+    float: Средняя оценка.
+    """
+
+    valid_grades = [grade for grade in grades if grade is not None]
+
+    return round(sum(valid_grades)/len(valid_grades), 3) if valid_grades else 0 
+
+def process_student_data(data):
+    """
+    Обрыбытывает данные студентов, заменяя отсутствующие оценки средним значением.
+
+    Args:
+    data (list of dict): Список словарей, каждый из которых содержит данные студента.
+
+    Returns:
+    list of dict: Обработаный список с даными студентов.
+    """
+
+    grades = [student.get("grade") for student in data]
+
+    average_grade = calculate_average_grade(grades)
+
+    print(average_grade)
+
+def main():
+    process_student_data(student_data)
+
+main()
+
+
+
+
+
+
