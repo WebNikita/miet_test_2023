d83176b1be3eba8e55c86eea51b949ec6c1e8b63
n.shvora <n.shvora@raceit.ru>
2023-12-19 19:26:49 +0300

    2 task done

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import csv
 student_data = [
     {"name": "Андрей", "surname": "Попов", "grade": 5, "project_id": "1"},
     {"name": "Степан", "surname": "Васильев", "grade": None, "project_id": "2"},
-    {"name": "Владимир", "surname": "Хадаров", "grade": 4, "project_id": "3"}
+    {"name": "Владимир", "surname": "Хадаров", "grade": 4, "project_id": "3"},
+    {"name": "Влад", "surname": "Хадаров", "grade": 2, "project_id": "4"},
+    {"name": "Глеб", "surname": "Орлов", "grade": 1, "project_id": "5"},
+    {"name": "Семен", "surname": "Архатов", "grade": 3, "project_id": "6"},
 ]
 
 def calculate_average_grade(grades):
@@ -32,7 +35,7 @@ def process_student_data(data):
     list of dict: Обработаный список с даными студентов.
     """
 
-    grades = [student.get("grade") for student in data]
+    grades = [student["grade"] for student in data]
 
     average_grade = calculate_average_grade(grades)
 
@@ -56,17 +59,87 @@ def save_to_csv(data, filename):
         writer.writeheader()
         writer.writerows(data)
 
-def main():
-    clear_data = process_student_data(student_data)
+def insertion_sort(array, key):
+    """
+    Алгоритм сортировки вставками.
+
+    Args:
+        array (list of dict): Список словарей, каждый из которых содержит данные студента.
+        key (str): Ключ по которому будет идти сортировка.
+
+    Returns:
+        list of dict: Остортированный список словарей.
+    """
+
+    for i in range(1, len(array)):
+        current_value = array[i]
+        position = i
+
+        while position > 0 and array[position - 1 ][key] > current_value[key]:
+            array[position] =  array[position - 1]
+            position -= 1
+        
+        array[position] = current_value
     
-    save_to_csv(clear_data, "student_new.csv")
+    return array[::-1]
 
-    for student in clear_data:
-        if student["name"] == "Владимир" and student["surname"] == "Хадаров":
-            vladimir_grade = student["grade"]
-            vladimit_project_id = student["project_id"]
 
-    print(f"Ты получил: {vladimir_grade}, за проект - {vladimit_project_id}")
+def get_top_students(array, count = 3):
+    """
+    Получаем топ студентов по оценкам.
+
+    Args:
+        array (list of dict): Отсортированый список словарей по убыванию оценок, каждый из которых содержит данные студента.
+        count (int): Кол-во студентов которые попадают в топ.
+
+    Returns:
+        list of dict: Список словарей с топ студентами.
+    """
+
+    return array[:count]
+
+def read_csv(file_path):
+    """
+    Читаем данны из файла в CSV формате.
+
+    Args:
+        file_path (str): Путь к файлу.
+    
+    Returns:
+        list of dict: Список словарей с топ студентами.
+    """
+
+    student_data = []
+
+    with open(file_path, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            student_data.append(row)
+            
+    return student_data
+    
+
+def main():
+    # clear_data = process_student_data(student_data)
+    
+    student_data_from_csv = read_csv("student.csv")
+
+    sort_student_data_from_csv = insertion_sort(student_data_from_csv, "grade")
+    top_students = get_top_students(sort_student_data_from_csv)
+
+    place = 1
+    for student in top_students:
+        print(f"{place} место: {student['name'][0]}. {student['surname']}")
+        place += 1
+
+    # save_to_csv(clear_data, "student.csv")
+
+    # for student in clear_data:
+    #     if student["name"] == "Владимир" and student["surname"] == "Хадаров":
+    #         vladimir_grade = student["grade"]
+    #         vladimit_project_id = student["project_id"]
+
+    # print(f"Ты получил: {vladimir_grade}, за проект - {vladimit_project_id}")
 
 main()
 
