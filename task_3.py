755e643fa3294b3a06054e42d67ed41e3f289822
n.shvora <n.shvora@raceit.ru>
2023-12-26 18:45:47 +0300

    Add function find_student

--- task_3.py
+++ task_3.py
@@ -1,3 +1,20 @@
+import csv
+
+
+def find_student(file_path,project_id):
+    flag = False
+    try:
+        with open(file_path, mode="r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["project_id"] == project_id:
+                    flag = True
+                    print(f"Проект № {project_id} делал: {row['name'][0]}.{row['surname']} он(а) получил(а) оценку - {row['grade']}")
+        if not flag:
+            print("Ничего не найдено.")    
+    except FileNotFoundError:
+        return "[ERROR] Файл не найден"
+
 def main():
     while True:
         user_input = input("Введите ID проекта или 'СТОП', для выхода: ")
@@ -5,5 +22,7 @@ def main():
             break
         elif not user_input.isdigit():
             print("Вы ввели строку, допускается только ввод числа, повторите попытку!")
+        else:
+            find_student("student.csv",user_input)
 
 main()
